746d3a78e336ba1cf421f974a062de77a0a78a82
Narasimha <chundurunarasimhadatta@outlook.com>
2026-04-03 09:50:37 +0530

    updated

---
+++ client/src/services/TodoService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+//default auth header
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+
+export const addTodo = async (data) => {
+    try {
+        const response = await axios.post('/api/todo/add', data);   
+        return response.data;
+    } catch (error) {
+        console.error('Error adding todo:', error);
+        throw error;
+    }
+};
+
+export const getTodos = async (userId) => {
+    try {
+        const response = await axios.get(`/api/todo/getAll/${userId}`);
+        return response.data.todos;
+    } catch (error) {
+        console.error('Error fetching todos:', error);
+        throw error;
+    }
+};
+
+export const deleteTodo = async (id) => {
+    try {
+        const response = await axios.post(`/api/todo/delete/${id}`);
+        return response.data;
+    }
+    catch (error) {
+        console.error('Error deleting todo:', error);
+        throw error;
+    }
+};
+
+export const updateTodo = async (id, data) => {
+    try {
+        const response = await axios.patch(`/api/todo/update/${id}`, data);
+        return response.data;
+    }
+    catch (error) {
+        console.error('Error updating todo:', error);
+        throw error;
+    }
+};
+
